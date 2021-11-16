f433d678c1172a3084e633c503a3b87281dbf5fc
rosseab-bit <roseab.developer@gmail.com>
2021-11-16 08:24:24 -0300

    add files conf and modules

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 import pymysql
 import sqlite3
 import jinja2
+import requests
+import secrets
+from routes import *
+from flask import Blueprint
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
-
-@app.route('/')
-def apiInfo():
-	info = '''<h1>Server bot informations</h1>
-	<p>This API need a number key for get info.</p>
-	<p>route: /api_server/id/option</p>
-	<p>Options: [status, stop, start]</p>'''
-	return info
-
-@app.route('/api_server/<string:id>/<string:option>', methods=['GET'])
-def infoServer(id, option):
-	if str(id) == 'batman':
-		return 'sucess'
-	else:
-		info = '''<h1>Server bot informations</h1>
-		<p>This API need a number key for get info.</p>
-		<p>Acces denegated, number key invalid</p>
-		<p>Options: [status, stop, start]</p>'''
-		return info
+app.register_blueprint(routes)
 
 
+server=json.loads(open('conf.d/server.json').read())
 if __name__ == '__main__':
-	app.jinja_env.filters['zip'] = zip
-	app.run(port = 3000, debug = True)
+        app.jinja_env.filters['zip'] = zip
+        app.run(host=server['server']['host'], port=server['server']['port'], debug = True)
